43fa305645f0c15c0a4199178f997bd74012da94
Mike-66 <mikemeyers@mein.gmx>
2021-08-15 18:56:18 +0200

    still buggy, still to save

--- main_async.cpp
+++ main_async.cpp
@@ -17,11 +17,11 @@ int main(int argc, char** argv)
     const char* port = argv[2];
     const char* dialogfile = argv[3];
 
-    ws_client_async wsc;
-    wsc.Init(host,port,dialogfile);
+    ws_client_async *wsc = new ws_client_async();
+    wsc->Init(host,port,dialogfile);
 
     do {
-        if(wsc.run(1000)!=0)
+        if(wsc->run(1000)!=0)
         break;
     }while(1);
 
